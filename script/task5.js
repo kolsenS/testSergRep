8ab7417ced60d8db43382d19778ef25ba9c15a20
Serg Kolsen <seleg2011@gmail.com>
2018-01-11 21:28:00 +0200

    initial project version

---
+++ script/task5.js
@@ -0,0 +1,26 @@
+﻿function OpText(str) {
+  str=str.replace(/[^A-Za-zА-Яа-яЁё\s]/g,' ');
+  stra=str.replace(/[^AaАа]/g,'');
+  str=str.replace(/ +/g,' ');
+  var mas=str.split(' ');
+  if (mas[0]==='') {
+     mas.splice(0,1);
+  };
+  if (mas[mas.length-1]==='') {
+     mas.pop ();
+  };
+  console.log('Количество слов в тексте: ',mas.length);
+  var masmidlength=mas.map(function(name) {
+    return name.length;
+  });
+  function sum(mas) {
+    return sum=mas.reduce(function(sm,it) {
+      return sm+it;
+    }, 0);
+  };
+  console.log('Средняя длина слова в тексте: ',sum(masmidlength)/mas.length);
+  (!!!stra) ? console.log('Символы из множества AaАа отсутствуют') : console.log('Текст содержит ',stra.length,' симв. из множества AaАа');
+};
+console.log('\n\n Задача: Работа с текстом\n\n\n');
+var st=prompt('Задайте предложение/словосочетание/абзац', '');
+OpText(st);
---
+++ script/task6.js
@@ -0,0 +1,31 @@
+﻿function range(pr1, pr2, pr3) {
+ //Проверяем, определена ли переменная, задающая шаг диапазона.
+ //Если не определена - принимаем шаг, равным 1.
+ (!!!pr3 && pr3!==0) ? pr3=+1 : pr3;
+ //Проверяем, направление вектора диапазона и направление шага.
+ //Если не совпадают или шаг равен 0 - выдаем сообщение о недопустимости заданых параметров.
+ if ((pr1>pr2 && pr3>0)  || (pr1<pr2 && pr3<0) || (pr3===0)) {
+   console.log ('Заданы недопустимые параметры');
+   return false;
+ } else {
+     var mas=[pr1];
+     for (var i=1;i<Math.floor((Math.abs(pr2-pr1)+1)/Math.abs(pr3));++i) {
+      (pr1<pr2) ? mas[i]=mas[i-1]+Math.abs(pr3) : mas[i]=mas[i-1]-Math.abs(pr3);
+     };
+     if ((mas[mas.length-1]+pr3)===pr2) {
+      mas.push(pr2);
+     };
+     return mas;
+   };
+};
+function sum(mas) {
+  return sum=mas.reduce(function(sm,it) {
+    return sm+it;
+  }, 0);
+};
+console.log('\n\n Задача: Диапазон\n\n\n');
+var p1=prompt ('Задайте начало диапазона:' , 1);
+var p2=prompt ('Задайте конец диапазона:', 10);
+var p3=prompt ('Задайте шаг диапазона:', 1);
+(range (+p1,+p2,+p3)===false) ? console.log('Массив не определен') :
+console.log(range (+p1,+p2,+p3)) + console.log(sum(range (+p1,+p2,+p3)));
