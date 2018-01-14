7caceef33bfafeb33ef61b3d0ee0f877d5fd86ca
Serg Kolsen <seleg2011@gmail.com>
2018-01-14 21:34:21 +0200

    14.01.2018 add task7

---
+++ script/task7.js
@@ -0,0 +1,13 @@
+﻿function randomNumbers () { 
+ var mas=[];
+ (function () {
+   while (mas.length<100) {
+    var item=Math.floor(Math.random()*101);
+    (mas.indexOf(item)===-1) ? mas[mas.length]=item : mas;
+   };
+ })();
+ return mas;
+};
+console.log(randomNumbers());
+
+
